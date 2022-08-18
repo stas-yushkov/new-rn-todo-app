1091b1f03118775cc8b4c8beaba67f60656b7dbf
stas-yushkov <stas.yushkov@gmail.com>
2022-08-19 02:11:08 +0300

    play with 'ScrollView'

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 
 import { Navbar, AddTodo, Todo } from './src';
 
@@ -8,7 +8,20 @@ import colors from './src/constants/colors';
 import { DEFAULT_COLOR_THEME } from './src/constants/';
 
 export default function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([
+    { id: 1, title: 1 },
+    { id: 2, title: 2 },
+    { id: 3, title: 3 },
+    { id: 4, title: 4 },
+    { id: 5, title: 5 },
+    { id: 6, title: 6 },
+    { id: 7, title: 7 },
+    { id: 8, title: 8 },
+    { id: 9, title: 9 },
+    { id: 10, title: 10 },
+    { id: 11, title: 11 },
+    { id: 12, title: 12 },
+  ]);
 
   const addTodo = (title) => {
     // const newTodo = {
@@ -39,13 +52,13 @@ export default function App() {
       <View style={styles.container}>
         <AddTodo onSubmit={addTodo} />
 
-        <View>
+        <ScrollView style={styles.scroll}>
           {todos.map(todo => {
             return (
               <Todo key={todo.id} todo={todo}></Todo>
             )
           })}
-        </View>
+        </ScrollView>
       </View>
     </View>
   );
@@ -60,4 +73,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
     paddingVertical: 20
   },
+  scroll: {
+    height: '50%'
+  }
 });
