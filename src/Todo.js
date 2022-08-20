3f068bbc797b4a5f446805572c4c427959544d35
stas-yushkov <stas.yushkov@gmail.com>
2022-08-20 03:04:56 +0300

    add onRemove.bind

--- src/Todo.js
+++ src/Todo.js
@@ -6,15 +6,11 @@ import { DEFAULT_COLOR_THEME, ACTIVE_OPACITY_NUM } from './constants';
 import colors from './constants/colors';
 
 export const Todo = ({ todo, onRemove }) => {
-  const longPressHandler = () => {
-    onRemove(todo.id)
-  }
-
   return (
     <TouchableOpacity
       activeOpacity={ACTIVE_OPACITY_NUM}
       onPress={() => console.log('Pressed: ', todo.title)}
-      onLongPress={longPressHandler}
+      onLongPress={onRemove.bind(null, todo.id)}
     >
       <View style={styles.todo}>
         <Text style={styles.text} key={todo.id}>{todo.title}</Text>
